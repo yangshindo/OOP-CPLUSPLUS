943e25ae8a8069223f6f858ea5e89c9012f3395c
João Mário <64316250+yangshindo@users.noreply.github.com>
2022-12-22 11:56:06 -0300

    Update list.hpp

--- list.hpp
+++ list.hpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-
 class List {
   private:
+
     Node *beginning;
     Node *end;
     int size;
@@ -13,10 +13,11 @@ class List {
     //GETTERS
     Node *getBeginning() {return this->beginning;}
     Node *getEnd() {return this->end;}
+    int getSize() {return this->size;}
 
     //Insere no início da lista
-    void insertAtTheBeginning() {
-      Node *node = new Node(rand() % 999);
+    void insertAtTheBeginning(int value) {
+      Node *node = new Node(value);
       if (this->size == 0) {
         this->beginning = node;
       } else {
@@ -25,6 +26,60 @@ class List {
         this->beginning->setNext(helper);
       }
       this->size++;
+    }
+
+
+    //Insere no fim da lista
+    void insertAtTheEnd(int value) {
+      Node *node = new Node(value);
+      if (this->size == 0) {
+        this->beginning = node;
+        this->end = node;
+      } else {
+        this->end->setNext(node);
+        this->end = node;
+      }
+      this->size++;
+    }
+
+  
+
+    //Remove de uma posição específica
+    int removeAt(int position) {
+
+      if (this->size == 0) {
+        return -1;
+      }
+
+      if (this->size == 1) {
+        Node *helper = this->beginning;
+        this->beginning = nullptr;
+        this->end = nullptr;
+        delete helper;
+        this->size--;
+      }
+
+      // link previous deleted node to next one
+      Node *lastNode;
+      Node *node = this->beginning;
+      for (int i = 0; i < this->size; i++) {
+        if (i == position) {
+          int value = node->getValue();
+          
+          if (i == 0) {
+            this->beginning = currentNode->getNext();
+          } else {
+            previousNode->setNext(currentNode->getNext());
+          }
+        }
+          delete node;
+          this->size--;
+          return value;
+        }
+        lastNode = node;
+        node = node->getNext();
+      }
+      return -1;
     }
 
 
@@ -57,20 +112,16 @@ class List {
       return value;
     }
 
-    
-
-
     //PRINT
     void print() {
       if (this->size > 0) {
         Node *node = this->beginning;
-        printf("\nInício da Lista:\n");
         for (int i = 0; i < this->size; i++) {
             printf("\n%d", node->getValue());
           node = node->getNext();
         }
       } else {
-        printf("Lista vazia.");
+        printf("Não há elementos.");
       }
     }
 };
